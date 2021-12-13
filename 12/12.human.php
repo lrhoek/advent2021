9c3e9082d2a50ff25f2bc81cd6f1ca8526f04b16
Laurens Hoek <lrhoek@gmail.com>
2021-12-13 20:03:49 +0100

    Day 12 Human Version Cleanup

--- 12/12.human.php
+++ 12/12.human.php
@@ -1,28 +1,30 @@
 <?php
 
 function map_connection(string $connection) : array {
-    $caves = explode('-', $connection);
-    return [$caves[0] => [$caves[1]], $caves[1] => [$caves[0]]];
+    list($source, $target) = explode('-', $connection);
+    return [$source => [$target], $target => [$source]];
 }
 
-function allowed_caves(array $caves, array $visited, bool $extended_rules) : array {
-    return array_values($extended_rules ? array_filter($caves, fn ($cave) => extended_rules($cave, $visited)) : array_filter($caves, fn ($cave) => simple_rules($cave, $visited)));
+function allowed_caves(array $caves, array $visited, int $maximum_single_small_cave_revisits) : array {
+    return array_filter($caves, fn ($cave) => cave_allowed($cave, $maximum_single_small_cave_revisits, $visited));
 }
 
-function simple_rules(string $cave, array $visited) : bool {
-    return !ctype_lower($cave) || !in_array($cave, $visited);
-}
-function extended_rules(string $cave, array $visited) : bool {
-    return !($cave === "start" || ctype_lower($cave) && in_array(2, array_count_values(array_filter($visited, ctype_lower(...)))) && in_array($cave, $visited));
+function cave_allowed($cave, $maximum_single_small_cave_revisits, $visited) : bool{
+    return !($cave === "start" || ctype_lower($cave) && in_array($maximum_single_small_cave_revisits, small_cave_amounts($visited)) && in_array($cave, $visited));
 }
 
-function get_number_of_paths($cave_system, $extended_rules = false, $start = 'start', $end = 'end', $visited = []) : int {
+function small_cave_amounts(array $caves) : array {
+    return array_count_values(array_filter($caves, ctype_lower(...)));
+}
+
+function get_number_of_paths($cave_system, $maximum_single_small_cave_revisits = 1, $start = 'start', $end = 'end', $visited = []) : int {
     $visited[] = $start;
-    return $start === $end ? 1 : array_reduce(allowed_caves($cave_system[$start], $visited, $extended_rules), fn ($total, $next) => $total + get_number_of_paths($cave_system, $extended_rules, $next, $end, $visited), 0);
+    $allowed = allowed_caves($cave_system[$start], $visited, $maximum_single_small_cave_revisits);
+    return $start === $end ? 1 : array_reduce($allowed, fn ($total, $next) => $total + get_number_of_paths($cave_system, $maximum_single_small_cave_revisits, $next, $end, $visited), 0);
 }
 
 $connections = explode(PHP_EOL, file_get_contents('input'));
 $cave_system = array_merge_recursive(...array_map(map_connection(...), $connections));
 
 echo get_number_of_paths($cave_system).PHP_EOL;
-echo get_number_of_paths($cave_system, true).PHP_EOL;
+echo get_number_of_paths($cave_system, 2).PHP_EOL;
